194f447de117fa807ef644730b6813f26e038995
DavidOno <DavidOno@users.noreply.github.com>
2022-01-28 14:43:08 +0100

    Improved Participant by adding explixit JSONProperties

--- src/main/kotlin/de/boettcher/alerter/core/register/Participant.kt
+++ src/main/kotlin/de/boettcher/alerter/core/register/Participant.kt
@@ -1,5 +1,6 @@
 package de.boettcher.alerter.core.register
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import lombok.ToString
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
@@ -9,11 +10,16 @@ import javax.validation.constraints.Email
 @Document(collection = "Participant")
 data class Participant(
     @Id
+    @JsonProperty("userId")
     val userId: Integer,
+    @JsonProperty("firstName")
     val firstName: String,
+    @JsonProperty("lastName")
     val lastName: String,
     @Email
+    @JsonProperty("email")
     val email: String,
+    @JsonProperty("privileges")
     val privileges: List<Privileges>
 ) {
 
